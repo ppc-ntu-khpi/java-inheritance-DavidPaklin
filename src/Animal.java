12f5aa760ba8bb4328acc7fb35be7522128d99fc
DavidPaklin <115375316+DavidPaklin@users.noreply.github.com>
2023-03-05 21:10:59 +0200

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,17 +1,46 @@
 public class Animal {
     protected String name;
     protected int weight;
-    public Animal() {
+
+/** 
+ *
+ * It is a constructor. 
+ *
+ */
+    public Animal() { 
+
         name = "generic animal";
         weight = 30;
     }
-    public void eat() {
+
+/** 
+ *
+ * Eat
+ *
+ */
+    public void eat() { 
+
         System.out.println("Animal eating");
     }
-    public void speak() {
+
+/** 
+ *
+ * Speak
+ *
+ */
+    public void speak() { 
+
         System.out.println("Animal speaking");
     }
-    public String toString() {
+
+/** 
+ *
+ * To string
+ *
+ * @return String
+ */
+    public String toString() { 
+
         return "\nMy animal:" + "\nName:\t" + name + "\nWeight:\t" + weight + " kg";
     }
 }
